aec88624979bebafbbac161d9e74920f1772b82e
thanh53199 <98029217+thanh53199@users.noreply.github.com>
2022-05-13 09:06:19 +0700

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+typeSpeed:100,
+backSpeed:90,
+loop:true,
+});
+// JavaScript Document
---
+++ main.js
@@ -0,0 +1,24 @@
+ mybutton = document.getElementById("btn-ontop");
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+	if(document.body.scrollTop > 200 || document.documentElement.scrollTop >200){
+		mybutton.style.display="block";
+		
+	}
+	else{
+		mybutton.style.display="none";
+	}
+}
+
+function topFunction(){
+	document.body.scrollTop=0;
+	document.documentElement.scrollTop=0;
+}
+
+var typed = new Typed('.typing', {
+strings: ["Web Designer.", "Code Developer.","Photographers."],
+typeSpeed:100,
+backSpeed:90,
+loop:true,
+});
+// JavaScript Document
